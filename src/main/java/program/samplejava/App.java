1ecd1c13bddc716df673f504b2fb7164ac3d6fba
kaushik <21bd1a05ancsef@gmail.com>
2023-11-21 12:27:11 +0530

    maven java push

---
+++ src/main/java/program/samplejava/App.java
@@ -0,0 +1,13 @@
+package program.samplejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
